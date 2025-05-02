6040354819bfd0c85ed30dcb0134ada3a7a7694b
Mubashar Hassan <58395173+mubasshar009@users.noreply.github.com>
2025-05-03 01:05:39 +0400

    Update layout.tsx
    Revert back to original

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,25 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* Add your Google Analytics script */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-2JQ4Z2GLF0"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-2JQ4Z2GLF0');
-            `,
-          }}
-        />
-      </head>
+      
       <body className={`${inter.className} ${orbitron.variable}`}>
         <AudioContextProvider>
           <SiteHeader />

166704f3f9ebaaccecf53eed1a43f54d9184cb2b
Mubashar Hassan <58395173+mubasshar009@users.noreply.github.com>
2025-05-03 00:59:00 +0400

    Update layout.tsx
    Added Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2JQ4Z2GLF0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2JQ4Z2GLF0');
+</script>
+      </head>
       <body className={`${inter.className} ${orbitron.variable}`}>
         <AudioContextProvider>
           <SiteHeader />
